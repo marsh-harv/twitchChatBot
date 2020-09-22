9225579d06ac3ecd14f242a9b439491be6616670
Mars <marshall.harvey@outlook.com>
2020-09-22 18:35:50 +0100

    update to es6

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,5 @@ export const channelName = '#marsjitz98';
 export const oAuthToken = 'oauth:ptc29sq9mz7nv868hz4jp6af1rbihl';
 
 export const botName = 'myChatBotEy';
+
+export const blockedWords = ['nigger', 'fuck', 'coon', 'n1gga', 'twat', 'shit', 'wanker', 'suck', 'dick'];
